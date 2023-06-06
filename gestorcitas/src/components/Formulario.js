a5fe0fd9c2962bb72b1a415def823a140f32b1d7
MARIANO CACERES SMOLER <46956818@ort>
2023-06-06 10:41:55 -0300

    terminar con cambios

--- gestorcitas/src/components/Formulario.js
+++ gestorcitas/src/components/Formulario.js
@@ -4,30 +4,50 @@ import { useState } from "react";
 
 export default function Formulario({onAgregarCita}) {
 
-    const [idState, setId] = useState(1);
-    const [mascotaState, setMascota] = useState('');
-    const [duenoState, setDueno] = useState('');
-    const [fechaState, setFecha] = useState('');
-    const [horaState, setHora] = useState('');
-    const [sintomasState, setSintomas] = useState('');   
+    const [idState, setId] = useState(0);
+    const [cita, setCita] = useState({
+        id : idState,
+        mascota:'',
+        dueno:'',
+        fecha:'',
+        hora:'',
+        sintomas:''
+    }); 
 
     function crearCita(e){
 
         e.preventDefault();
+
+        if(cita.mascota == '' || cita.dueno == '' || cita.fecha == '' || cita.hora == '' || cita.sintomas == ''){
+            
+        }
         setId(idState + 1)
 
-        let cita = {
+        //validar campos
+        //crear cita
+        //vaciar form
+
+        setCita({
             id: idState,
-            mascota: mascotaState,
-            dueno: duenoState,
-            fecha: fechaState,
-            hora:horaState,
-            sintomas:sintomasState
-        } 
+            mascota: e.target.mascota.value,
+            dueno: e.target.propietario.value,
+            fecha: e.target.fecha.value,
+            hora: e.target.hora.value,
+            sintomas: e.target.sintomas.value
+        })
 
         onAgregarCita(cita)
 
     }
+
+    const onHandleChange = (e) =>{
+        setCita({
+            ...cita,
+            [e.target.name]:e.target.value
+        }           
+        )
+    }
+
 
     return (
     <>
@@ -41,15 +61,15 @@ export default function Formulario({onAgregarCita}) {
             }       */}
 
             <label>Nombre Mascota</label>
-            <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" onChange={(e) => setMascota(e.target.value)}/>
+            <input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" value={cita.mascota} onChange={onHandleChange}/>
             <label>Nombre Dueño</label>
-            <input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" onChange={(e) => setDueno(e.target.value)}/>
+            <input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" value={cita.dueno} onChange={onHandleChange}/>
             <label>Fecha</label>
-            <input type="date" name="fecha" className="u-full-width" onChange={(e) => setFecha(e.target.value)}/>
+            <input type="date" name="fecha" className="u-full-width" value={cita.fecha} onChange={onHandleChange}/>
             <label>hora</label>
-            <input type="time" name="hora" className="u-full-width" onChange={(e) => setHora(e.target.value)}/>
+            <input type="time" name="hora" className="u-full-width" value={cita.hora} onChange={onHandleChange}/>
             <label>Sintomas</label>  
-            <textarea name="sintomas" className="u-full-width" onChange={(e) => setSintomas(e.target.value)}></textarea>      
+            <textarea name="sintomas" className="u-full-width" value={cita.sintomas} onChange={onHandleChange}></textarea>      
 
             <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
 
